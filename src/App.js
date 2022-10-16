66bb52e81a951bef589549084b05170b97daf926
Stephen Werbeck <SWerbeck@yahoo.com>
2022-10-16 16:56:39 -0400

    got list to show

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import SingleTodo from './components/SingleTodo';
+
+
+let toDos = [
+  { id: 1, toDo: "Walk the garabage", completed: "false" },
+  { id: 2, toDo: "Cook the laundry", completed: "false" },
+  { id: 3, toDo: "Slaughter your enemies", completed: "false" },
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>To Do List</h1>
+     <SingleTodo toDos={toDos}/>
+     <SingleTodo toDos={toDos}/>
+    {/* {toDos.map(todo => <SingleTodo toDos={toDos}/>)} */}
     </div>
   );
 }
---
+++ src/components/SingleTodo.jsx
@@ -0,0 +1,21 @@
+import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers'
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+
+const SingleTodo = ({toDos}) => {
+    console.log(toDos)
+
+    return (
+        <div>
+            {toDos.map(todo => <p key={todo.id}>{todo.toDo}</p>)}
+        </div>
+    )
+}
+
+
+
+
+
+
+export default SingleTodo
